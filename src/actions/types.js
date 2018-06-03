94e97adb8ea080fc01e8f79e4d7b2da168d25747
kkarab <kostiskarampelas@yahoo.com>
2018-06-03 19:19:12 +0200

    added action types

---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+export const CHOOSE_BASE = 'CHOOSE_BASE';
+export const CHOOSE_SAUCE = 'CHOOSE_SAUCE';
+export const ADD_TOPPING = 'ADD_TOPPING';
+export const REMOVE_TOPPING = 'REMOVE_TOPPING';
+export const CHOOSE_DRONE = 'CHOOSE_DRONE';
+
