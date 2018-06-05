081a889bd1e60cdfa1760f05ede96a9e4b48549e
kkarab <kostiskarampelas@yahoo.com>
2018-06-05 16:23:41 +0200

    created orderCost component

---
+++ src/components/OrderCost.js
@@ -0,0 +1,29 @@
+const BaseCost = {
+    "NY20" : 6.45,
+    "NY25" : 8.99,
+    "NY30" : 11.49,
+    "NY35" : 13.49
+}
+
+const SauceCost = {
+    "White sauce" : Number(0),
+    "Red sauce" : Number(0),
+    "Double red sauce" : 1.00,
+    "Mix it up" : 1.50 
+}
+
+const ToppingCost = {
+    "Cherry tomatoes" : 0.50,
+    "Red onions" : 0.50,
+    "Green olives" : 0.50,
+    "Corn" : 0.50,
+    "Spinach" : 0.50,
+    "Pineapple" : 0.50,
+    "Chicken" : 0.50
+}
+
+const DeliveryCost = {
+    "Drone" : 1.1,
+    "Normal" : 1
+}
+
